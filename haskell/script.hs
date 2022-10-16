1acfc54b65ddcd854267a3f3b1d9d030b77a933f
Yui Nishizawa <yui.nishizawa@gmail.com>
2022-10-17 06:34:36 +0900

    More haskell

--- haskell/script.hs
+++ haskell/script.hs
@@ -1,11 +1,23 @@
 -- import System.Random
-
+import Data.Functor.Identity
+import Data.Functor.Compose
+import Data.Functor.Sum
+import Data.Functor.Product
+import Data.Functor.Const
+import Data.Foldable
+import Data.Traversable
+import Data.List
+import Data.Function
+import Data.Char
 import Control.Arrow
 import Control.Monad
 import Control.Applicative
 
 main = callTest (do
-  testFold
+  -- testPatterBind
+  -- testWrapped
+  -- testFunctorMonad
+  -- testFold
   -- testMonad
   -- testTraverse
   -- testTraverse2
@@ -15,21 +27,10 @@ main = callTest (do
   -- testRandom
   -- testTypeSyntax
   -- testCase
-  -- testDo
+  -- testDoExpression
   -- testData
-  -- testArrow
+  testArrow
   ) "main"
-
-type TestType a = [a]
-data TestType2 a = List [a] | Point (a,a)
-data TestType3 a = List2 ([] a) | Point2 (a,a)
-
--- f(x) = (x+1)(x+3) - x
-f :: Num a => a -> a
-f = do
-  y <- (+1)
-  z <- (+3)
-  (y*z+)
 
 assert :: Bool -> ()
 assert cond =
@@ -40,13 +41,51 @@ assertIO cond message = do
   print message
   unless cond undefined
 
-testDone = print "done"
+testDone = putStrLn "done"
+
+printBanner msg = putStrLn $ "=====" ++ msg ++ "====="
+
+printList :: (Foldable f, Show a) => f a -> IO ()
+printList = traverse_ print 
 
 callTest :: IO () -> String -> IO ()
 callTest x message = do
-  print $ "start:" ++ message
+  putStrLn $ "start:" ++ message
   x
-  print $ "end:" ++ message
+  putStrLn $ "end:" ++ message
+
+-- TEST TEMPLATE
+testTemplate = callTest (do
+  let x = 1
+  print "copy me"
+  testDone) "testTemplate"
+
+testList = callTest (do
+  print $ uncons [1]
+  print $ uncons ([]::[Int])
+  let x:xs = [1,2]
+  let Just (x,xs) = uncons $ x:xs
+
+  print $ find (==2) [2..10]
+  print $ lookup 1 [(1,2),(2,3)]
+  print $ lookup 2 [(1,2),(1,3)]
+  print $ partition (<2) [1..10]
+  print $ intersperse 5 [10..20]
+  print $ transpose [[1,2,3],[4..6]]
+  print $ transpose [[1,2,3],[4..7]]
+
+  print $ subsequences [1..3]
+  testDone) "testList"
+
+testFunctorMonad = callTest (do
+  let a = Identity 1
+  let l = InL $ Just 1
+  let r = InR $ Just 2 
+
+  let printSum = traverse_ print
+  printSum l
+  printSum r
+  testDone) "testFunctionMonad"
 
 testMonad = callTest (do
   do 
@@ -148,28 +187,88 @@ testCompositionMonad = callTest (do
 
 
 testFunctionMonad = callTest (do 
+  let f = (*2)
   print $ f 3
   testDone) "testFunctionMonad"
 
 testArrow = callTest (do
+  -- Basic
   let f = (+4) >>> (*3)
   print $ f 1
   print $ f 2
+
+  -- Conditional
+  let f :: Either Int Char -> Either Int Char
+      f = left (*3)
+  let g :: Either Int Char -> Either Int Char
+      g = right $ chr . (+1) .ord
+  print $ f $ Left 1
+  print $ f $ Right 'a'
+  print $ g $ Left 1
+  print $ g $ Right 'a'
+
+  let f = (+2) +++ (+3)
+  print $ f $ Left 1
+  print $ f $ Right 1
+  let f = (+2) ||| ord
+
+  print $ f $ Left 1
+  print $ f $ Right 'a'
   testDone
             ) "testArrow"
 
+testArrowLoop = callTest (do
+  -- loop 
+  printBanner "Loop test"
+
+  -- an equivalent implementation of loop
+  let loop2 f x = let
+          f1 = fst.f
+          f2 = snd.f
+        in f1 (x, fix (\b -> f2 (x,b)))
+
+
+  let factorialFix fact n = if n <= 1 then n else n * fact (n-1)
+  let factorialLoop (n, f) = (f n, factorialFix f)
+  let factorialLoop2 (n, f) = (f n 1, g f)
+        where g f i accum = if i <= 0 then accum else f (i-1) (i * accum)
+
+  let fibonacchiFix fib n = if n <= 1 then n else fib (n-1) + fib (n-2)
+  let fibonacci (n, f) = (f n, fibonacchiFix f)
+
+  let testLoop f testName = do
+          printBanner $ "start:" ++ testName
+          printList $ f <$> [1..10]
+          printBanner $ "done:" ++ testName
+
+  testLoop (loop factorialLoop2) "loop+factorialLoop2"
+  testLoop (loop2 factorialLoop2) "loop2+factorialLoop2"
+
+  testLoop (fix factorialFix) "fix+factorialFix"
+  testLoop (loop factorialLoop) "loop+factorialLoop"
+  testLoop (loop2 factorialLoop) "loop2+factorialLoop"
+
+  testLoop (fix fibonacchiFix) "fix+fibonacci0"
+  testLoop (loop fibonacci) "loop+fibonacci"
+  testLoop (loop2 fibonacci) "loop2+fibonacci"
+  testDone
+            ) "testArrowLoop"
+
+
+data TestType3 a = List2 ([] a) | Point2 (a,a)
 testData = callTest (do
   let x = List2 [1] 
   let y = Point2 (1,10)
   testDone
            ) "testData"
 
-testDo = callTest (do
+
+testDoExpression = callTest (do
   print "Enter name"
   x <- getLine
   print $ "Hello " ++ x
   testDone
-         ) "testDo"
+         ) "testDoExpression"
 
 testCase = callTest (do 
   let f x = case x of 
@@ -203,18 +302,28 @@ testCase = callTest (do
 --   _ -> "other"
 
 
-testPatterBind = let
-  f x 
-    | x == 1 = "one"
-    | x == 2, x < 10 = "two"
-    | let n = "other", n == "a" = n
-    | otherwise = "other"
-  g x y
-    | n:_ <- x, m:_ <- y, n == m = show n
-    | otherwise = "other"
-    where k = 100; s = 2
-          l = 10
-  in g [1] [2]
+testPatterBind = callTest (do
+  let f x
+        | x == 1 = "one"
+        | x == 2, x < 10 = "two"
+        | let n = "other", n == "a" = n
+        | otherwise = "other"
+  let g x y
+        | n:_ <- x, m:_ <- y, n == m = "x[0] == y[0] == " ++ show n
+        | otherwise = "other"
+        where k = 100; s = 2
+              l = 10
+
+  printBanner "f"
+  print $ f 1
+  print $ f 2
+  print $ f 3
+
+  printBanner "g"
+  print $ g [1] [1]
+  print $ g [1] [2]
+  testDone) "testPatterBind"
+
 
 testPatternMatch = let
   k@[n,_,_] = [1,2,3]
@@ -233,3 +342,10 @@ testTypeSyntax = callTest (do
   testDone
                  ) "testTypeSyntax"
 
+testWrapped = callTest (do
+  let x = (WrapMonad $ Just 1) :: WrappedMonad Maybe Int
+  let x = WrapMonad [1] :: WrappedMonad [] Int
+  let f = WrapArrow (*2)
+  print $ (unwrapArrow $ (+) <$> f <*> f) 2
+  print $ unwrapMonad $ (*2) <$> x
+  testDone) "testWrapped"
