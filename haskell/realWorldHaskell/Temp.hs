6d17ac0639fb612132c0bed43e87dacb7c9f3f40
Yui Nishizawa <yui.nishizawa@gmail.com>
2022-11-09 11:51:12 +0900

    haskell: cleanup Real World Haskell

---
+++ haskell/realWorldHaskell/Temp.hs
@@ -0,0 +1,5 @@
+module Temp (
+  run,
+  ) where
+
+run = putStrLn "Temp"
---
+++ haskell/realWorldHaskell/main.hs
@@ -0,0 +1,7 @@
+import qualified Chapter15 
+import qualified Temp
+
+main :: IO ()
+main = do
+  Temp.run
+  Chapter15.run
