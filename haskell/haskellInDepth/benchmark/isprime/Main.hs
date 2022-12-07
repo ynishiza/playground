948cbed5888fe660a0566debfd1164bebd13a763
Yui Nishizawa <yui.nishizawa@gmail.com>
2022-12-07 12:40:23 +0900

    haskell: iplookup main

--- haskell/haskellInDepth/benchmark/isprime/Main.hs
+++ haskell/haskellInDepth/benchmark/isprime/Main.hs
@@ -1,15 +1,17 @@
-import Prime
 import Criterion.Main
 import Criterion.Types
+import Prime
 
 pn :: Integer
 pn = 16183
+
 -- pn = 102982987
 
 main :: IO ()
-main = defaultMainWith (defaultConfig { reportFile = Just "bench.prime.html" } ) [
-  bench "isPrime (list)" (whnf isPrime pn),
-  bench "isPrimeV2 (list)" (whnf isPrimeV2 pn),
-  bench "isPrime" (whnf isPrime1 pn)
-                   ]
-
+main =
+  defaultMainWith
+    (defaultConfig {reportFile = Just "bench.prime.html"})
+    [ bench "isPrime (list)" (whnf isPrime pn),
+      bench "isPrimeV2 (list)" (whnf isPrimeV2 pn),
+      bench "isPrime" (whnf isPrime1 pn)
+    ]
--- haskell/haskellInDepth/src/ip/lookup/Main.hs
+++ haskell/haskellInDepth/src/ip/lookup/Main.hs
@@ -1,5 +1,46 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
+import Data.Foldable
+import Data.List.Extra
+import qualified Data.Text as T
+import Fmt
 import IPLookup
+import IPParse
+import Options.Applicative
 
-main = undefined
+main :: IO ()
+main = do
+  Params {..} <- execParser parserInfo
+  (Right db) <- parseIPRangeDB . T.pack <$> readFile dbPath
+  case ips of
+    Right x -> run db x
+    Left e -> error $ e ||+ ""
+
+run :: IPRangeDB -> [IP] -> IO ()
+run db = traverse_ f
+  where
+    f ip = fmtLn $ ip ||+ ":" +| lookupIP db ip |+ ""
+
+data Params = Params
+  { dbPath :: !FilePath,
+    ips :: !(Either String [IP])
+  }
+  deriving (Show)
+
+parserInfo :: ParserInfo Params
+parserInfo = info (helper <*> parser) (fullDesc <> progDesc "")
+
+parser :: Parser Params
+parser =
+  Params
+    <$> strOption (long "db" <> short 'd')
+    <*> (tryParseIP <$> strOption (long "ips" <> short 'i'))
+
+tryParseIP :: String -> Either String [IP]
+tryParseIP = traverse p . split (== ',')
+  where
+    p t = case parseIP (T.pack $ trim t) of
+      Just x -> Right x
+      Nothing -> Left $ "Failed to parse IP:" +| t |+ ""
