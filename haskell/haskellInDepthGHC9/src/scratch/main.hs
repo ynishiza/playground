b215476f71ce195d45dffcbca2bfb205d5950926
Yui Nishizawa <yui.nishizawa@gmail.com>
2023-09-12 05:57:20 +0900

    Move scratch to https://github.com/ynishiza/haskell-notes

--- haskell/haskellInDepthGHC9/src/scratch/main.hs
+++
@@ -1,6 +0,0 @@
--- Run with 
---  stack exec -- src/scratch/<name>.hs
---  stack ghci -- src/scratch/<name>.hs
-main :: IO ()
-main = pure ()
-
--- haskell/haskellInDepthGHC9/src/scratch/template.hs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env stack
-{-
-  Run with
-    stack exec -- src/scratch/<name>.hs
-    stack ghci -- src/scratch/<name>.hs
--}
-
-main :: IO ()
-main = putStrLn "Hello"
